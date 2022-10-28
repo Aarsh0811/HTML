7ef13a39c77f840614d8b30ba18e58505201297b
Aarsh0811 <114701316+Aarsh0811@users.noreply.github.com>
2022-10-28 01:01:16 -0400

    Create custom-js.js

---
+++ FINAL_PROJECT/custom-js.js
@@ -0,0 +1,14 @@
+//  NAME: AARSH PARIMAL PATEL
+//  STUDENT ID: 200520260
+//  FINAL PROJECT
+
+
+// Here I have added the script to load the header and footer to all web pages.
+$(document).ready(function () {
+    $(function () {
+        $("#header").load("header.html");
+        $("#footer").load("footer.html");
+    });
+});
+
+  // end of js
